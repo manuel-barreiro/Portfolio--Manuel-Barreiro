9f8b3faf69d7203441751489cf0361cc3d3aca47
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-04-25 22:35:24 -0300

    done

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$formcontent="
+    Nombre: $name \n
+    E-mail: $email \n
+    Mensaje: $message
+";
+
+$recipient = "manuel.barreiro@ing.austral.edu.ar";
+
+$subject = "Consulta - $name";
+
+$header = "From: $email \r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8";
+mail($recipient, $subject, $formcontent, $header) or die("Error!");
+header("Location: index.html");
+
+?>
